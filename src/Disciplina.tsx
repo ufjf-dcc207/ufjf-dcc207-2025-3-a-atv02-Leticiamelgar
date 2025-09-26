f394e4d0af2e7e3283199aa51750678af56bbdba
Leticia <leticia.melgar@estudante.ufjf.br>
2025-09-25 22:27:32 -0300

    Primeiro commit

---
+++ src/Disciplina.tsx
@@ -0,0 +1,15 @@
+import "./Disciplina.css";
+
+type DisciplinaProps = {
+    codigo: string;
+    nome: string;
+}
+
+export default function Disciplina(props: DisciplinaProps) {
+    return (
+        <div className="disciplina">
+            <h1>{props.codigo}</h1>
+            <h2>{props.nome}</h2>
+        </div>
+    )
+}
